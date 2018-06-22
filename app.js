c7df6ac4cc82da0eeaf4ecf222ed17a73cdeef51
michael albert <kasmickleva@gmail.com>
2018-06-22 14:10:35 +0100

    modify: create a server using express

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 
+const app = express();
+
+// use middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to Ride-My-Way Api');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Welcome to Ride-My-Way Api, this route does not exist!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+export default app;
