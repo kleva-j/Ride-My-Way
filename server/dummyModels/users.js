c6472da5a6e3d66d6a7573f09cef1707441b72a2
michael albert <kasmickleva@gmail.com>
2018-06-22 16:35:50 +0100

    feat: create ride controllers to get all rides

---
+++ server/dummyModels/users.js
@@ -0,0 +1,44 @@
+const users = [
+  {
+    id: 1,
+    firstname: 'Monica',
+    lastname: 'Ada',
+    email: 'Monica@ada.com',
+    gender: 'female',
+    password: 'monicada'
+  },
+  {
+    id: 2,
+    firstname: 'Casandra',
+    lastname: 'Chuma',
+    email: 'casa@dra.com',
+    gender: 'female',
+    password: 'and#show'
+  },
+  {
+    id: 3,
+    firstname: 'Osuji',
+    lastname: 'Kelechi',
+    email: 'osuji@kelechi.com',
+    gender: 'M',
+    password: 'anima@c#lay'
+  },
+  {
+    id: 4,
+    firstname: 'jeff',
+    lastname: 'bezos',
+    email: 'jeff@bezos.com',
+    gender: 'M',
+    password: 'jeffbezos'
+  },
+  {
+    id: 5,
+    firstname: 'Brown Sindy',
+    lastname: '',
+    email: 'brown@sindy.com',
+    gender: 'M',
+    password: 'brownsindy'
+  }
+];
+
+export default users;
---
+++ server/middlewares/RideMiddleware.js
@@ -0,0 +1,27 @@
+import model from '../dummyModels/index';
+
+const { rides } = model;
+/**
+ * @class BusinessMiddleware
+ * @classdesc Implements user being able to get businesses by location and category
+ */
+class RideMiddleware {
+  /**
+   * Get all rides
+   *
+   * @static
+   * @param {object} req - The request object
+   * @param {object} res - The response object
+   * @param {function} next - The next middleware
+   * @return {object} Message and data of the rides
+   * @memberof RideMiddleware
+   */
+  static getAllRides(req, res) {
+    res.status(200).send({
+      message: 'Gotten all rides successfully',
+      data: rides
+    });
+  }
+}
+
+export default RideMiddleware;
