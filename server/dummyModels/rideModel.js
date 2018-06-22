fc2f47746fb0a6b2224b6360187e4ef2e68f52cc
michael albert <kasmickleva@gmail.com>
2018-06-22 14:45:40 +0100

    chore: add dummy data for APIs

---
+++ server/dummyModels/rideModel.js
@@ -0,0 +1,73 @@
+const rides = [
+  {
+    From: 'Yaba',
+    Destination: 'Oshodi',
+    Driver: {
+      firstname: 'Monoca',
+      lastname: 'Ada',
+      gender: 'female'
+    },
+    Time: '8:00 am',
+    Date: '25 - 07 - 2018',
+    Pasengers: [
+      {
+        firstname: 'Ayo',
+        lastname: 'Tolu',
+        gender: 'male'
+      },
+      {
+        firstname: 'casandra',
+        lastname: 'Chuma',
+        gender: 'female'
+      }
+    ]
+  },
+  {
+    From: 'Maryland',
+    Destination: 'Obanikoro',
+    Driver: {
+      firstname: 'casandra',
+      lastname: 'Chuma',
+      gender: 'female'
+    },
+    Time: '8:00 am',
+    Date: '26 - 07 - 2018',
+    Pasengers: [
+      {
+        firstname: 'casandra',
+        lastname: 'Chuma',
+        gender: 'female'
+      },
+      {
+        firstname: 'Monoca',
+        lastname: 'Ada',
+        gender: 'female'
+      }
+    ]
+  },
+  {
+    From: 'Yaba',
+    Destination: 'Maryland',
+    Driver: {
+      firstname: 'Nelson',
+      lastname: 'kelechi',
+      gender: 'male'
+    },
+    Time: '8:00 am',
+    Date: '25 - 07 - 2018',
+    Pasengers: [
+      {
+        firstname: 'casandra',
+        lastname: 'Chuma',
+        gender: 'female'
+      },
+      {
+        firstname: 'Justin',
+        lastname: 'Osuji',
+        gender: 'male'
+      }
+    ]
+  }
+];
+
+export default rides;
