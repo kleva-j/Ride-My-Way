269b12329a999688b4aaba27c24f97f7d4911764
michael albert <kasmickleva@gmail.com>
2018-06-23 13:56:10 +0100

    feat: create an authenticated endpoint POST: /rides

--- server/routes/ridesRoute.js
+++ server/routes/ridesRoute.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import RideMiddleware from '../middlewares/RideMiddleware';
+import RideController from '../controllers/RideController';
 
 const {
   getAllRides, getByDateAndDest, getByDate, getByDestination
 } = RideMiddleware;
 
+const { createRideOffer } = RideController;
+
 const rideRouter = express.Router();
 
 rideRouter.route('/rides')
+  .post(createRideOffer)
   .get(getByDate, getByDestination, getByDateAndDest, getAllRides);
 
 export default rideRouter;
