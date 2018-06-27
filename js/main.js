136a2f10efdcc2aa0b91f0148149c850fb7f81d1
kleva-j <kasmickleva@gmail.com>
2018-06-27 18:17:24 +0100

    fix: replace ride offers created with ride request

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-window.onload = function(){
+window.onload = (function(){
   toggleNavBar();
   toggleSideBar();
-
-};
+})();
 
 function toggleNavBar(){
   let burgar_nav = document.querySelector('.burgar-nav');
@@ -21,28 +20,36 @@ function toggleNavBar(){
 };
 
 function toggleSideBar(){
-  let [profile, rideoffer, rideHistory] = document.querySelectorAll(".nav");
+  let [,profile, rideoffer, ridesTaken, ridesGiven] = document.querySelector('.resize').children;
 
-  rideHistory = addEventListener("click", showProfile)
+  ridesTaken = addEventListener("click", showProfile);
   profile.addEventListener("click", showProfile);
-  rideoffer = addEventListener("click", showProfile)
-}
+  rideoffer = addEventListener("click", showProfile);
+  ridesGiven = addEventListener("click", showProfile);
+;}
 
 function showProfile(e) {
   let details = document.querySelector(".detail");
   let offer = details.nextElementSibling;
   let listings = offer.nextElementSibling;
+  let listings2 = offer.nextElementSibling;
 
-  if(e.target.innerText == "Profile"){
+  if(e.target.innerText == "Dashboard"){
     details.style.display = "block";
   }
-  else if(e.target.innerText == "Ride offers created"){
+  else if(e.target.innerText == "Ride Requests"){
     offer.style.display = "inline-block";
     details.style.display = "none";;
   }
-  else if(e.target.innerText == "Ride History"){
+  else if(e.target.innerText == "Rides Given"){
     listings.style.display = "inline-block";
     details.style.display = "none";
     offer.style.display = "none";
+  }
+  else if(e.target.innerText == "Rides Taken"){
+    listings2.style.display = "inline-block";
+    details.style.display = "none";
+    offer.style.display = "none";
+    listings.style.display = 'none';
   }
 }
