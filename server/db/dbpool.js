96b82aebbf17214deb26d5edccc884738aff39c6
kleva-j <kasmickleva@gmail.com>
2018-07-03 18:16:47 +0100

    style: add configuration to .env file

--- server/db/dbpool.js
+++ server/db/dbpool.js
@@ -1,18 +1,24 @@
-import pg from 'pg';
+import { Pool } from 'pg';
 
-const URL = 'postgres://vhzmtdts:y8TA6YamhS8wUnvzwxNo-1vN9uth2nAp@baasu.db.elephantsql.com:5432/vhzmtdts';
-var conString = process.env.ELEPHANTSQL_URL || URL;
-var client = new pg.Client(conString);
-client.connect((err) => {
+const pool = new Pool({
+    connectionString: process.env.ONLINE_DB_URL
+});
+
+
+pool.query('CREATE TABLE usrs', (err, response) => {
   if(err) {
-    return console.error('could not connect to postgres', err);
+    return console.error('could not create table', err);
   }
-  client.query('SELECT NOW() AS "theTime"', (err, result) => {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
-  });
+  if(response) {
+      console.log('User sucessfully created')
+  }
+//   client.query('SELECT NOW() AS "theTime"', (err, result) => {
+//     if(err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log(result.rows[0].theTime);
+//     //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+
+    pool.end();
+
 });
