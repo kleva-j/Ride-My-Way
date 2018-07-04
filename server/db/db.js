513320e29b95d69b874158da3cf4c93e8e5f0b6b
kleva-j <kasmickleva@gmail.com>
2018-07-04 15:56:24 +0100

    fix: fix refusal to connect to database

--- server/db/db.js
+++ server/db/db.js
@@ -7,4 +7,5 @@ db.connect({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS
-})
+});
+
--- server/db/dbpool.js
+++ server/db/dbpool.js
@@ -1,24 +1,18 @@
-import { Pool } from 'pg';
+import pg from 'pg';
 
-const pool = new Pool({
-    connectionString: process.env.ONLINE_DB_URL
+const pool = new pg.Pool({
+  connectionString: process.env.ONLINE_DB_URL
 });
 
 
-pool.query('CREATE TABLE usrs', (err, response) => {
-  if(err) {
-    return console.error('could not create table', err);
-  }
-  if(response) {
-      console.log('User sucessfully created')
-  }
-//   client.query('SELECT NOW() AS "theTime"', (err, result) => {
-//     if(err) {
-//       return console.error('error running query', err);
-//     }
-//     console.log(result.rows[0].theTime);
-//     //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+pool.connect((err, client, done) => {
+  if (err) return done(err);
 
-    pool.end();
-
+  pool.query('SELECT $1::text as name', ['pg-pool'], (err, res) => {
+    done();
+    if (err) {
+      return console.error('query error', err.message, err.stack);
+    }
+    console.log('hello from', res.rows[0].name);
+  });
 });
