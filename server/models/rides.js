9a28f8015e8b98c6853908a26d25a21dd3e81711
Michael <kasmickleva@gmail.com>
2018-09-18 00:33:46 +0100

    refactor: fix eslint warnings

--- server/models/rides.js
+++ server/models/rides.js
@@ -3,25 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let connectionString = "";
+let connectionString = '';
 
 if (process.env.NODE_ENV === 'test') {
   connectionString = process.env.DB_TEST;
-}
-else if (process.env.NODE_ENV === 'development') {
+} else if (process.env.NODE_ENV === 'development') {
   connectionString = process.env.DB_DEV;
-}
-else { connectionString = process.env.DB_URL }
+} else { connectionString = process.env.DB_URL; }
 
-let sql = `
+const sql = `
 DROP TABLE IF EXISTS rides;
-CREATE TABLE rides(id SERIAL PRIMARY KEY, location VARCHAR(255) NOT NULL, destination VARCHAR(255) NOT NULL, slot INTEGER NOT NULL, time TIME NOT NULL, user_id UUID REFERENCES users(id) ,driver VARCHAR(255) NOT NULL, carModel VARCHAR(255) NOT NULL, takeOffDate DATE NOT NULL, status VARCHAR(255) DEFAULT 'pending', riders TEXT[] DEFAULT '{}', created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), updated_at TIMESTAMPTZ);
+CREATE TABLE rides(id SERIAL PRIMARY KEY, location VARCHAR(255) NOT NULL, destination VARCHAR(255) NOT NULL, seats INTEGER NOT NULL, time TIME NOT NULL, user_id UUID REFERENCES users(id), driver VARCHAR(255) NOT NULL, carModel VARCHAR(255) NOT NULL, takeOffDate DATE NOT NULL, status VARCHAR(255) DEFAULT 'pending', riders TEXT[] DEFAULT '{}', created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), updated_at TIMESTAMPTZ);
 `;
 
 const client = new Client(connectionString);
 client.connect();
 client.query(sql, (err) => {
-  if(err) {
+  if (err) {
     client.end();
     console.log(err.stack);
   }
