773f386e6e6bfddd7e6b907a9c3b97ab7178dd9d
Michael <kasmickleva@gmail.com>
2018-09-18 00:19:13 +0100

    style: edit import statement

--- config/config.js
+++ config/config.js
@@ -1,20 +1,18 @@
-import pg from pg;
-import dotenv from dotenv;
+import pg from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-let connectionString = "";
+let connectionString = '';
 
 if (process.env.NODE_ENV === 'test') {
-  connectionString = process.env.DB_TEST
-}
-else if (process.env.NODE_ENV === 'development') {
-  connectionString = process.env.DB_DEV
-}
-else { connectionString = process.env.DB_URL }
+  connectionString = process.env.DB_TEST;
+} else if (process.env.NODE_ENV === 'development') {
+  connectionString = process.env.DB_DEV;
+} else { connectionString = process.env.DB_URL; }
 
 const pool = pg.Pool({ connectionString });
 
 export default (callback) => {
   pool.connect((err, client, done) => callback(err, client, done));
-}
+};
