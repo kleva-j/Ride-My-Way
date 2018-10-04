133a081d2c615fa4074bce8a052bce478ffb0e2d
Michael <kasmickleva@gmail.com>
2018-10-04 11:54:40 +0100

    chore: use express-validator, cors, jsend and morgan middleware

--- app.js
+++ app.js
@@ -1,40 +1,63 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import logger from 'morgan';
+import path from 'path';
 import jsend from 'jsend';
 import cors from 'cors';
-import morgan from 'morgan';
-
-import rideRouter from './server/routes/ridesRoute';
-import userRouter from './server/routes/userRoute';
+import expressValidator from 'express-validator';
+import routers from './server/routes/index';
 
 const app = express();
-const logger = createLogger('RideMyWay');
 
+app.use(express.static(path.join(__dirname, '/template')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const corsOptions = {
-  optionSuccessStatus: 200,
-};
-app.options('*', cors(corsOptions));
-app.use('*', cors(corsOptions));
-if (process.env.NODE_ENV === 'DEVELOPMENT') {
-  app.use(morgan('combined'));
+app.use(jsend.middleware);
+app.use(cors());
+
+if (process.env.NODE_ENV !== 'TEST') {
+  app.use(logger('dev'));
 }
 
-app.use('/api/v1', rideRouter);
-app.use('/api/v1/auth', userRouter);
-// app.use('api/v1/users')
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+    const namespace = param.split('-');
+    const root = namespace.shift();
+    let formParam = root;
+    while (namespace.length) {
+      formParam += `[ ${namespace.shift()} ]`;
+    }
+    return { param: formParam, msg, value };
+  }
+}));
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to Ride-My-Way Api');
 });
 
-app.all('*', (req, res) => res.status(404).jsend.fail({ message: 'wrong endpoint: visit api with api/v1/rides' }));
+routers(app);
+
+// Handle 404
+app.use((req, res, next) => {
+  res.status(404);
+  res.sendFile(path.join(__dirname, 'template', '404.html'));
+  next();
+});
+
+// Handle Errors
+app.use((err, req, res) => {
+  console.error('An application error has occurred:');
+  console.error(err.stack);
+  res.status(500).jsend.error({ message: 'page not found' });
+  res.sendFile(path.join(__dirname, 'template', '500.html'));
+});
+
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  logger.info(`app listening on port ${port} ...`);
+  console.log(`App listening on port ${port}`);
 });
 
 export default app;
